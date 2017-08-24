c7724b686edb0e8831144cd8429fde90c9c7ff31
jherson onairam <jherson@MacBook-Pro-de-jherson.local>
2017-08-24 17:49:34 -0500

    simple init

---
+++ js/main.js
@@ -0,0 +1,12 @@
+/* google maps autocomplete */ 
+var defaultBounds = new google.maps.LatLngBounds(
+  new google.maps.LatLng(-12.114194,-77.044559),
+  new google.maps.LatLng(-12.114194,-77.044559));
+
+var input = document.getElementById('auto-c');
+var options = {
+  bounds: defaultBounds,
+  types: ['establishment']
+};
+
+autocomplete = new google.maps.places.Autocomplete(input, options);
